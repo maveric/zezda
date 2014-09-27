8e0fe0717dee9cf0c472bb243863c4e8630b6a9e
maveric <maveric100@gmail.com>
2014-09-27 15:51:24 -0400

    Update CandlePullController.php

--- src/Zezda/ExchangeBundle/Controller/CandlePullController.php
+++ src/Zezda/ExchangeBundle/Controller/CandlePullController.php
@@ -1,5 +1,27 @@
+<?php
 /*
 Controller to pull candles from available sources so we can keep a record in our own local database
 for the purpose of looking up alert stocks at any time in the past
 */
 
+namespace Zezda\ExchangeBundle\Controller;
+
+//Symfony extensions and such
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Finder\Finder;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Filesystem\Exception\IOExceptionInterface;
+
+
+//Uses from our own created library
+use Zezda\ExchangeBundle\Form\FileUploadType;
+use Zezda\ExchangeBundle\Entity\Stock;
+use Zezda\ExchangeBundle\Entity\Candle;
+
+class ParseController extends Controller
+{
+}
+?>
